6b9cdc13071c1a9d09b57951be8610430b69d22a
sanderboompje <120382015+sanderboompje@users.noreply.github.com>
2023-11-27 14:30:31 +0100

    js werkt( niet mooi)

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
-// JavaScript Document
 console.log("hi");
+
+function showSection(sectionNumber) {
+    for (let i = 1; i <= 6; i++) {
+        const section = document.getElementById(`section${i}`);
+        if (i === sectionNumber) {
+            section.classList.add('visible');
+        } else {
+            section.classList.remove('visible');
+        }
+    }
+}

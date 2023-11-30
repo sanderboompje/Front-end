98f09d4056594f21b6b14ac8dc9768e6338a7ebc
sanderboompje <120382015+sanderboompje@users.noreply.github.com>
2023-11-30 10:36:14 +0100

    buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,13 @@
-console.log("hi");
+// Ik heb ChatGPT gebruikt voor hulp bij deze function en Sem
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('button[id^="button"]');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            const sectionNumber = parseInt(event.target.id.replace('button', ''));
+            showSection(sectionNumber);
+        });
+    });
+});
 
 function showSection(sectionNumber) {
     for (let i = 1; i <= 6; i++) {
